3efafe88d50af0858b707e74ca06f41648a02dad
Akarmain <101408699+akarmain@users.noreply.github.com>
2024-06-02 19:42:07 +0300

    add MWP

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
+import asyncio
 import os
 
 from dotenv import load_dotenv
 from telethon import TelegramClient
 
 load_dotenv()
-
 api_id = os.getenv('API_ID')
 api_hash = os.getenv('API_HASH')
-client = TelegramClient('anon', api_id, api_hash)
 
 
-async def main():
-    me = await client.get_me()
-    print(me.stringify())
-    print(me.phone)
-    await client.send_message("me", '200 ok')
+async def send_message(receiver, txt_msg):
+    async with TelegramClient('anon', api_id, api_hash) as client:
+        await client.send_message(receiver, txt_msg)
 
 
 if __name__ == '__main__':
-    with client:
-        client.loop.run_until_complete(main())
+    user_name_or_id = "akarmain"
+    text_msg = 'Hello World!\n\n autor @akarmain'
+    asyncio.run(send_message(user_name_or_id, text_msg))
